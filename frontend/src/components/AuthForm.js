0f488f80f05f3cfb1584e90783ccfbfa20f0ace9
Ahmed Khalifa <ahmedkhalifa201395@yahoo.com>
2024-06-10 21:15:03 +0300

    Refactor

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,8 +1,6 @@
 import {
   Form,
   Link,
-  json,
-  redirect,
   useActionData,
   useNavigation,
   useSearchParams,
@@ -52,38 +50,3 @@ function AuthForm() {
 }
 
 export default AuthForm;
-
-export async function action({ request }) {
-  const searchParams = new URL(request.url).searchParams;
-  const mode = searchParams.get("mode") || "login";
-
-  if (mode !== "login" && mode !== "signup") {
-    throw json({ message: "Unsupported mode." }, { status: 422 });
-  }
-
-  const data = await request.formData();
-  const authData = {
-    email: data.get("email"),
-    password: data.get("password"),
-  };
-
-  const url = "http://localhost:8080/" + mode;
-
-  const response = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(authData),
-  });
-
-  if (response.status === 422 || response.status === 401) {
-    return response;
-  }
-
-  if (!response.ok) {
-    throw json({ message: `Could not ${mode}.` }, { status: 500 });
-  }
-
-  return redirect("/");
-}
