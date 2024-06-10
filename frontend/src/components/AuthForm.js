fe295a9550166f69271d6680093e6abcc2581cf7
Ahmed Khalifa <ahmedkhalifa201395@yahoo.com>
2024-06-10 19:53:44 +0300

    Add authentication action

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,4 +1,4 @@
-import { Form, Link, useSearchParams } from "react-router-dom";
+import { Form, Link, json, redirect, useSearchParams } from "react-router-dom";
 
 import classes from "./AuthForm.module.css";
 
@@ -30,3 +30,38 @@ function AuthForm() {
 }
 
 export default AuthForm;
+
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get("mode") || "login";
+
+  if (mode !== "login" && mode !== "signup") {
+    throw json({ message: "Unsupported mode." }, { status: 422 });
+  }
+
+  const data = await request.formData();
+  const authData = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+
+  const url = "http://localhost:8080/" + mode;
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(authData),
+  });
+
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json({ message: `Could not ${mode}.` }, { status: 500 });
+  }
+
+  return redirect("/");
+}
